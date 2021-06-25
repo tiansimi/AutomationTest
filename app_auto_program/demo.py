f63827ff9e295b0358f8db3dee61dda0ed4c9fa5
tiansimi <970280628@qq.com>
2021-06-25 14:33:33 +0800

    启动appium Demo

--- app_auto_program/demo.py
+++ app_auto_program/demo.py
@@ -1,5 +1,5 @@
 from appium import webdriver
-
+from appium.webdriver.common.mobileby import MobileBy
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
 desired_caps['platformVersion'] = '10.0'
@@ -11,3 +11,8 @@ desired_caps['udid'] = '2529c2e6'
 
 # 连接Appium server
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+
+driver.find_element_by_id()
+driver.find_element_by_class_name()
+driver.find_element_by_accessibility_id()   # content-desc
+driver.find_element_by_android_uiautomator('new UiSelector().text("柠檬社区")') # 柠檬社区要用双引号，因为是Java语言
